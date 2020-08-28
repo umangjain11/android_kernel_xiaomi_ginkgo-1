c6adadd56a39e621610b0edd960fcfd62b05c300
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-08-28 11:44:50 +0530

    sultan_bench: little cluster
    Change-Id: Idffd87ad87d19fcad519bca82bc1c4ed638f76dd

--- kernel/sultan_bench.c
+++ kernel/sultan_bench.c
@@ -12,7 +12,7 @@
 
 /**************************** CONFIGURATION BEGIN ****************************/
 static const int big_cpu_freqs[] = {
-#if 1
+#if 0
 	300000,
 	652800,
 	902400,
@@ -25,7 +25,7 @@ static const int big_cpu_freqs[] = {
 };
 
 static const int little_cpu_freqs[] = {
-#if 0
+#if 1
 	300000,
 	614400,
 	864000,
